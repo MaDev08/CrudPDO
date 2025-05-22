4724cd6ee2b26f4abc6feb677d1b7b93878907d8
Mateus Aureliano <mateusudo32@gmail.com>
2025-05-22 11:30:31 -0300

    ...

--- database/connect.php
+++ database/connect.php
@@ -5,9 +5,9 @@ $password = "";
 $database = "pdo_php";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  $pdo = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
   //ALERTA O ERRO, CASO OCORRA
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connexão bem-sucedida!!!";
 } catch(PDOException $e) {
   echo "Erro: " . $e->getMessage();
